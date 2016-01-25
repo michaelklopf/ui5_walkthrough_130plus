2668576761ead03d47c2a2909cd8f9a5c937d5a0
Klopf Michael <m.klopf@fis-gmbh.de>
2016-01-25 16:30:26 +0100

    Formatting list item statuses. Introducing formatter function

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "mk/tutorial/model/formatter"
+], function(Controller, JSONModel, formatter) {
   "use strict";
   return Controller.extend("mk.tutorial.controller.InvoiceList", {
+    formatter: formatter,
     onInit: function() {
       var viewModel = new JSONModel({
         currency: "EUR"
