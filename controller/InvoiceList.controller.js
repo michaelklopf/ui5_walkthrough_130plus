3f187d2b114a48681ac8d0849e0e35d42cfe5ce7
Michael Klopf <klopf.michael@gmail.com>
2016-01-25 22:23:29 +0100

    Filtering, sorting, grouping

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel",
-  "mk/tutorial/model/formatter"
-], function(Controller, JSONModel, formatter) {
+  "mk/tutorial/model/formatter",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
   "use strict";
   return Controller.extend("mk.tutorial.controller.InvoiceList", {
     formatter: formatter,
@@ -11,6 +13,16 @@ sap.ui.define([
         currency: "EUR"
       });
       this.getView().setModel(viewModel, "view");
+    },
+    onFilterInvoices: function(event) {
+      var filter = [];
+      var query = event.getParameter("query");
+      if (query) {
+        filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+      }
+      var list = this.getView().byId("invoiceList");
+      var binding = list.getBinding("items");
+      binding.filter(filter);
     }
   });
 });
