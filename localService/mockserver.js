347b9c424511efe2b2fd8286951b8e4b81ed6d96
Klopf Michael <m.klopf@fis-gmbh.de>
2016-01-26 15:31:59 +0100

    Adding mock server with automatic model instantiation - not working properly due to lack of proxy though

---
+++ localService/mockserver.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+  "sap/ui/core/util/MockServer"
+], function(MockServer) {
+  "use strict";
+  return {
+    init: function() {
+      var mockServer = new MockServer({
+        rootUri: "/destinations/northwind/V2/Northwind/Northwind.svc/"
+      });
+      var uriParameters = jQuery.sap.getUriParameters();
+
+      MockServer.config({
+        autoRespond: true,
+        autoRespondAfter: uriParameters.get("serverDelay") || 1000
+      });
+
+      var path = jQuery.sap.getModulePath("mk.tutorial.localService");
+      mockServer.simulate(path + "/metadata.xml", path + "/mockdata");
+      mockServer.start();
+    }
+  };
+});
