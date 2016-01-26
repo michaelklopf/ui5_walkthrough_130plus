a3793ad5d5e101e1656b6b339731d1d70cb7d89f
Klopf Michael <m.klopf@fis-gmbh.de>
2016-01-26 15:33:41 +0100

    Making oData model for standard app work again

--- Component.js
+++ Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
   "sap/ui/core/UIComponent",
   "sap/ui/model/json/JSONModel",
-  "mk/tutorial/controller/WelcomeDialog"
-], function (UIComponent, JSONModel, WelcomeDialog) {
+  "mk/tutorial/controller/WelcomeDialog",
+  "sap/ui/model/odata/v2/ODataModel"
+], function (UIComponent, JSONModel, WelcomeDialog, ODataModel) {
   "use strict";
   return UIComponent.extend("mk.tutorial.Component", {
     metadata: {
@@ -19,6 +20,10 @@ sap.ui.define([
       };
       var model = new JSONModel(data);
       this.setModel(model);
+
+      var config = this.getMetadata().getConfig();
+      var invoiceModel = new ODataModel(config.invoiceRemote);
+      this.setModel(invoiceModel, "invoice");
 
       this.welcomeDialog = new WelcomeDialog();
     }
