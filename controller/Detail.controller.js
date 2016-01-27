fbf5a02a46a60ff2941451ffefe061e30e1cc81a
Klopf Michael <m.klopf@fis-gmbh.de>
2016-01-27 17:32:24 +0100

    Routing with going back functionality

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function(Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/routing/History"
+], function(Controller, History) {
   "use strict";
   return Controller.extend("mk.tutorial.controller.Detail", {
     onInit: function() {
@@ -13,6 +14,17 @@ sap.ui.define([
         model: "invoice"
       });
       console.log(this.getView());
+    },
+    onNavBack: function() {
+      var history = History.getInstance();
+      var previousHash = history.getPreviousHash();
+
+      if (previousHash !== undefined) {
+        window.history.go(-1);
+      } else {
+        var router = sap.ui.core.UIComponent.getRouterFor(this);
+        router.navTo("overview", true);
+      }
     }
   });
 });
